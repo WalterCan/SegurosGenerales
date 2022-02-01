2af98eeffa88fb757e67c1b4ab88357fc709463a
WalterCan <walterdaniel1316@gmail.com>
2022-02-01 14:42:17 -0300

    Header Finish

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(done) {
-	src("../SegurosGenerales/src/scss/app.scss") //Ubicamos el Archivo
+	src("../SegurosGenerales/src/scss/**/*.scss") //Ubicamos el Archivo
+		.pipe(plumber())
 		.pipe(sass()) //Lo compilamos
-		.pipe(dest("/build/css")); //Almacenamos en el disco duro
+		.pipe(dest("build/css")); //Almacenamos en el disco duro
 
 	done();
 }
 // Creamos el Watch
 function dev(done) {
-	watch("../SegurosGenerales/src/scss/app.scss", css); //dos parametros, Identificamos el archivo y que tarea vamos a mandar a llamar
-	// pipe(dest("/build/css"));
+	watch("../SegurosGenerales/src/scss/**/*.scss", css); //dos parametros, Identificamos el archivo y que tarea vamos a mandar a llamar
 	done();
 }
 exports.css = css;
